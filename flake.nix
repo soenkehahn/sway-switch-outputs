2eaf43a4b69fe37b1cc18b6ab7be1fc48a985e75
Sönke Hahn <soenkehahn@gmail.com>
2023-08-27 13:39:45 +0200

    Add rust hello world app

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          src = ./.;
+        in
+        {
+          packages = {
+            default = pkgs.rustPlatform.buildRustPackage {
+              name = "switch-outputs";
+              src = src;
+              cargoLock = {
+                lockFile = "${src}/Cargo.lock";
+              };
+            };
+          };
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              self.packages.${system}.default.buildInputs
+              pkgs.cargo
+            ];
+          };
+          apps = {
+            default = flake-utils.lib.mkApp {
+              drv = self.packages.${system}.default;
+            };
+          };
+        }
+      );
+}
